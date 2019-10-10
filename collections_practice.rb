6972256d492436c623d4a3f55ef9068043665433
Geoff Elliott <tinywv@gmail.com>
2019-10-10 21:05:03 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,51 @@
 
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort {|a, b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+  array.sort
+  array[0], array[1], array[2] = array [0], array[2], array[1]
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  kesha_array = []
+  array.each do |word|
+    array = word.split("")
+    array[2] = "$"
+    kesha_array << array.join
+  end
+  kesha_array
+end
+
+def find_a(array)
+  array.select do |word|
+    word.start_with?("a")
+  end
+end
+
+def sum_array(array)
+  array.inject { |sum, n| sum + n}
+end
+
+def add_s(array)
+  array.each_with_index.collect do |word, index|
+    if index == 1
+      word
+    else
+      word << "s"
+    end
+  end
+end
